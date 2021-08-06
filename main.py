e51167257574b8717f7c07b4dc03d853604e10bb
flowers186 <62859441+dvisionst@users.noreply.github.com>
2021-08-06 18:36:28 +0000

    Added the commented code for the Reeborg Maze Challenge.

--- main.py
+++ main.py
@@ -42,3 +42,50 @@
 #         move()
 #     elif wall_in_front():
 #         jump()
+
+#This code is for use with Reeborg's World
+#This is for Hurdle challenge 4
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+# def jump():
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+    
+# while at_goal() is False:
+#     if right_is_clear():
+#         jump()
+#     elif wall_in_front():
+#         turn_left()
+#     elif wall_on_right():
+#         move()
+#     else:
+#         move()
+
+
+
+#This code is for use with Reeborg's World
+#This is for the maze challenge
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# while not at_goal():
+#    if front_is_clear() and right_is_clear():
+#     turn_right()
+#     move()
+#    elif is_facing_north() and front_is_clear():
+#     move()
+#    elif right_is_clear() and wall_in_front():
+#     turn_right()
+#     move()
+#    elif front_is_clear():
+#     move()
+#    elif wall_in_front() and wall_on_right():
+#     turn_left()
+#     if front_is_clear():
+#         move()
