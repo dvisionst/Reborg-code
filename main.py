e6dd41f652e581739a8eab1aeb134847af98501c
flowers186 <62859441+dvisionst@users.noreply.github.com>
2021-08-06 03:12:46 +0000

    Commented out the Reeborg Code. Turned it all into comments to show it's not meant to run on Replit as is. Has to be used on reeborg.ca

--- main.py
+++ main.py
@@ -1,19 +1,44 @@
-def turn_right():
-    turn_left()
-    turn_left()
-    turn_left()
-def jump():
-    move()
-    turn_left()
-    move()
-    turn_right()
-    move()
-    turn_right()
-    move()
-    turn_left()
+#This code is for use with Reeborg's World
+#This is for Hurdle challenge 2
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+# def jump():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
     
     
     
-while at_goal() is False:
-    jump()
+# while at_goal() is False:
+#     jump()
     
+#This code is for use with Reeborg's World
+#This is for Hurdle challenge 3
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+# def jump():
+    
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+    
+    
+    
+# while at_goal() is False:
+#     if front_is_clear():
+#         move()
+#     elif wall_in_front():
+#         jump()
